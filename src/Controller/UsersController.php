79fa2c4c034e7cba057d25f57929039334f27236
ervin <molinarervin@gmail.com>
2020-10-17 13:38:54 +0800

    ervin

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -103,4 +103,17 @@ class UsersController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
+    public function insertUsers(){
+        $user = $this->Users->newEntity();
+            if ($this->request->is('post')) {
+                $user = $this->Users->patchEntity($user, $this->request->getData());
+                if ($this->Users->save($user)) {
+                    $this->Flash->success(__('The user has been saved.'));
+
+                    return $this->redirect(['action' => 'index']);
+                }
+                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            }
+            $this->set(compact('user'));
+    }
 }
---
+++ src/Template/Users/insertUsers.ctp
@@ -0,0 +1,15 @@
+<div class="users form large-9 medium-8 columns content">
+    <?= $this->Form->create($user) ?>
+    <fieldset>
+        <legend><?= __('Add User') ?></legend>
+        <?php
+            echo $this->Form->control('first_name');
+            echo $this->Form->control('last_name');
+            echo $this->Form->control('email');
+            echo $this->Form->control('password');
+            echo $this->Form->control('contact_no');
+        ?>
+    </fieldset>
+    <?= $this->Form->button(__('Submit')) ?>
+    <?= $this->Form->end() ?>
+</div>
